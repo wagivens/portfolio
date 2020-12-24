5844a3b51b69d205fc48169b8d9e7658d6194f9f
Will Givens <wagivens@usc.edu>
2020-12-23 17:14:07 -0800

    Updated UI

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+elements = {
+    memoji: document.querySelector('.header-nav__memoji'),
+}
+
+function randomMemoji() {
+    let randomNumber = Math.ceil(Math.random() * 3);
+    elements.memoji.setAttribute('src', `memoji/memoji-${randomNumber}.png`);
+}
+
 window.addEventListener('load', function () {
     document.body.classList.add('fadeIn');
 });
+
+randomMemoji();
