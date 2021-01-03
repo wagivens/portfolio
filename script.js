84d0aa8414bc619eda74f1f45d32138a070018f3
Will Givens <wagivens@usc.edu>
2021-01-02 21:10:33 -0800

    Added alt descriptions to images

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ elements = {
 function randomMemoji() {
     let randomNumber = Math.ceil(Math.random() * 3);
     elements.memoji.setAttribute('src', `memoji/memoji-${randomNumber}.png`);
+    elements.memoji.setAttribute('alt', 'MeMoji photo of Will Givens IV')
 }
 
 window.addEventListener('load', function () {
