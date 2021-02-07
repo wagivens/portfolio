e72dd6af1d356b4eb2e35d314f7bd57b5af21aab
Will Givens <wagivens@usc.edu>
2021-02-06 16:24:12 -0800

    Style: Redesigned the home page's project gallery.

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-elements = {
-    memoji: document.querySelector('.header-nav__memoji')
-}
-
-function randomMemoji() {
-    let randomNumber = Math.ceil(Math.random() * 3);
-    elements.memoji.setAttribute('src', `memoji/memoji-${randomNumber}.png`);
-    elements.memoji.setAttribute('alt', 'MeMoji photo of Will Givens IV')
-}
+'use strict';
 
 window.addEventListener('load', function () {
     document.body.classList.add('fadeIn');
 });
-
-randomMemoji();
