31591232ee1f1c1766453d205d9bac068f413282
Will Givens <wagivens@usc.edu>
2021-04-02 11:50:39 -0700

    Add intrapage navigation.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 "use strict";
 
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
   document.body.classList.add("fadeIn");
+});
+
+/* Intrapage Navigation Functionality: 
+For each intrapage-nav__link, add a listener for a click event. When the
+link is clicked, grab its text content and set it as the variable sectionID.
+Lastly, use the sectionID to scroll the resepctive section into view.
+*/
+let intranavLinks = document.querySelectorAll('.intrapage-nav__link');
+
+intranavLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    let sectionID = link.textContent;
+    document.getElementById(sectionID).scrollIntoView({behavior: 'smooth'});
+  });
 });
