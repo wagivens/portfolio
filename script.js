06383ff5b9e5b644e0236cfb8e5451d229420c26
Willard Givens IV <willard.givensiv@gmail.com>
2021-10-08 14:34:14 -0700

    Update UI

--- script.js
+++ script.js
@@ -1,65 +1,116 @@
-"use strict";
+import CREDENTIALS from "./credentials.js";
 
-window.addEventListener("load", () => {
-  document.body.classList.add("fadeIn");
-});
+("use strict");
 
 const DOMelements = {
-  caseStudySection: document.querySelector('.case-study__section:first-of-type'),
-  caseStudyImages: document.querySelectorAll('.main-image'),
-  scrollToTop: document.querySelector('.scroll-to-top')
+  handEmoji: document.querySelector(".handwave"),
+  caseStudySection: document.querySelector(
+    ".case-study__section:first-of-type"
+  ),
+  caseStudyImages: document.querySelectorAll(".main-image"),
+  scrollToTop: document.querySelector(".scroll-to-top"),
+};
+
+function waveHand() {
+  DOMelements.handEmoji.animate(
+    [
+      { transform: "rotateZ(0deg)" },
+      { transform: "rotateZ(30deg)" },
+      { transform: "rotateZ(0deg)" },
+      { transform: "rotateZ(30deg)" },
+      { transform: "rotateZ(0deg)" },
+    ],
+    1000
+  );
 }
+
+setInterval(waveHand, 3000);
+
+// Moving an image forward and backward based on scroll direction.
+let transformXIncrement = 0;
+let currentYPosition = window.pageYOffset;
+// window.addEventListener('scroll', () => {
+//   const image = DOMelements.msgsImage;
+//   let newYPosition = window.pageYOffset;
+
+//   // image.animate([
+//   //   {transform: `translateX(${transformXIncrement++}px)`}
+//   // ], 1);
+
+//   // transformXIncrement++;
+//   // currentYPosition = window.pageYOffset;
+
+//   if (newYPosition < currentYPosition) {
+//     image.style.transform = `translateX(${transformXIncrement--}px)`;
+//     transformXIncrement--;
+//     currentYPosition = window.pageYOffset;
+//   }
+
+//   if (newYPosition > currentYPosition) {
+//     image.style.transform = `translateX(${transformXIncrement++}px)`;
+//     transformXIncrement++;
+//     currentYPosition = window.pageYOffset;
+//   }
+// })
 
 /* Intrapage Navigation Functionality: 
 For each intrapage-nav__link, add a listener for a click event. When the
 link is clicked, grab its text content and set it as the variable sectionID.
 Lastly, use the sectionID to scroll the resepctive section into view. */
-let intranavLinks = document.querySelectorAll('.intrapage-nav__link');
+let intranavLinks = document.querySelectorAll(".intrapage-nav__link");
 intranavLinks.forEach((link) => {
-  link.addEventListener('click', () => {
+  link.addEventListener("click", () => {
     let sectionID = link.textContent;
-    document.getElementById(sectionID).scrollIntoView({behavior: 'smooth'});
+    document.getElementById(sectionID).scrollIntoView({ behavior: "smooth" });
   });
 });
 
 // Scroll-to-Top Button
-document.querySelector('.scroll-to-top').addEventListener('click', () => {
-  document.querySelector('.intrapage-nav').scrollIntoView({behavior: 'smooth'});
-});
+if (document.querySelector(".scroll-to-top"))
+  document.querySelector(".scroll-to-top").addEventListener("click", () => {
+    document
+      .querySelector(".intrapage-nav")
+      .scrollIntoView({ behavior: "smooth" });
+  });
 
 // Setting up Intersection Observer for Image Lazy-Loading
-const imageObserver = function(list) {
-  let elementObserver = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.style.visibility = 'visible';
+const imageObserver = function (list) {
+  let elementObserver = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) entry.target.style.visibility = "visible";
         observer.unobserve(entry.target);
-      }
-    });
-    }, {
+      });
+    },
+    {
       root: null,
       threshold: 0.1,
-      rootMargin: '0px'
+      rootMargin: "0px",
     }
   );
 
-  list.forEach(item => {
+  list.forEach((item) => {
     elementObserver.observe(item);
   });
-}
+};
 
-const sectionObserver = new IntersectionObserver(intersectionEntries => {
-  intersectionEntries.forEach(entry => {
-    if (entry.isIntersecting || window.pageYOffset < entry.boundingClientRect.y) {
-      DOMelements.scrollToTop.classList.add("hidden");
-    } else {
-      DOMelements.scrollToTop.classList.remove("hidden");
-    }
-  });
-  }, {
+const sectionObserver = new IntersectionObserver(
+  (intersectionEntries) => {
+    intersectionEntries.forEach((entry) => {
+      if (
+        entry.isIntersecting ||
+        window.pageYOffset < entry.boundingClientRect.y
+      ) {
+        DOMelements.scrollToTop.classList.add("hidden");
+      } else {
+        DOMelements.scrollToTop.classList.remove("hidden");
+      }
+    });
+  },
+  {
     root: null,
     threshold: 0,
-    rootMargin: "0px"
+    rootMargin: "0px",
   }
 );
 
@@ -67,4 +118,46 @@ const sectionObserver = new IntersectionObserver(intersectionEntries => {
 imageObserver(DOMelements.caseStudyImages);
 
 // We want to observe the second Case Study Section
-sectionObserver.observe(DOMelements.caseStudySection);
+if (DOMelements.caseStudySection)
+  sectionObserver.observe(DOMelements.caseStudySection);
+
+// async function getTwitterUserID() {
+//   try {
+//     const getUserInfo = await fetch(
+//       `${CREDENTIALS.API_URL}by/username/willgivensiv`,
+//       {
+//         headers: {
+//           Authorization:
+//             "Bearer AAAAAAAAAAAAAAAAAAAAAO%2FkQwEAAAAA3rQEpWBo%2Fnfzpybtk7kIZCyNxCg%3DCsSYVh93JLabK1ryWNijrf5p8P2rJw48LO7DCb0rUKDxMgDZw8",
+//         },
+//       }
+//     )
+//       .then((response) => response.json)
+//       .then((data) => console.log(data));
+//     if (getUserInfo.res.ok === "False") throw new Error(getUserInfo.error);
+//   } catch (error) {
+//     console.error(Error);
+//   }
+// }
+
+// getTwitterUserID();
+
+// console.log(CREDENTIALS);
+
+// const getTwitterFeed = async function () {
+//   let apiCall = await fetch(CREDENTIALS.API_URL);
+// };
+
+// function getUserInfo() {
+//   let apiCall = fetch(`${CREDENTIALS.API_URL}by/username/willgivensiv`, {
+//     headers: {
+//       Authorization:
+//         "Bearer AAAAAAAAAAAAAAAAAAAAAO%2FkQwEAAAAA3rQEpWBo%2Fnfzpybtk7kIZCyNxCg%3DCsSYVh93JLabK1ryWNijrf5p8P2rJw48LO7DCb0rUKDxMgDZw8",
+//     },
+//     method: "GET",
+//   });
+
+//   apiCall.then((response) => response.json).then((data) => console.log(data));
+// }
+
+// getUserInfo();
